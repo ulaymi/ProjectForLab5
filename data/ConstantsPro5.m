c63f598d734743aea13604c781d888e9936f35d7
Ilya Komarov <ilya.komarov.04@bk.ru>
2024-04-30 13:37:48 +0300

    Changed files

--- data/ConstantsPro5.m
+++ data/ConstantsPro5.m
@@ -1,28 +1,28 @@
 
 % Input values
-offset=1;
-angle_1=pi/2;
-angle_2=pi/2;
+ProjectForLab5.InputValues.offset=1;
+ProjectForLab5.InputValues.angle_1=pi/2;
+ProjectForLab5.InputValues.angle_2=pi/2;
 
-T=0.044;
+ProjectForLab5.Period.T=0.044;
 
 % Constants for the first submodel
-k11=1950;
-k12=78;
-k13=0.05;
-k14=0.275;
-k15=200;
+ProjectForLab5.FirstSubmodel.k11=1950;
+ProjectForLab5.FirstSubmodel.k12=78;
+ProjectForLab5.FirstSubmodel.k13=0.05;
+ProjectForLab5.FirstSubmodel.k14=0.275;
+ProjectForLab5.FirstSubmodel.k15=200;
 
 % Constants for the second submodel
-k21=2330;
-k22=94;
-k23=0.043;
-k24=2.8;
-k25=117;
+ProjectForLab5.SecondSubmodel.k21=2330;
+ProjectForLab5.SecondSubmodel.k22=94;
+ProjectForLab5.SecondSubmodel.k23=0.043;
+ProjectForLab5.SecondSubmodel.k24=2.8;
+ProjectForLab5.SecondSubmodel.k25=117;
 
 % Constants for the third submodel
-k31=3380;
-k32=156;
-k33=0.044;
-k34=2.94;
-k35=96;
+ProjectForLab5.ThirdSubmodel.k31=3380;
+ProjectForLab5.ThirdSubmodel.k32=156;
+ProjectForLab5.ThirdSubmodel.k33=0.044;
+ProjectForLab5.ThirdSubmodel.k34=2.94;
+ProjectForLab5.ThirdSubmodel.k35=96;
--- scripts/Initialisation/load_consts.m
+++ scripts/Initialisation/load_consts.m
@@ -1,3 +1,3 @@
 % Upload constant
 
-ConstantsPro5
+ConstantsPro5;
